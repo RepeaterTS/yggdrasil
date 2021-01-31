b3ffd7c1ba5fe36144d12eedefd23f48cdd07825
Jordan Jones <jordpjones@gmail.com>
2021-01-30 23:38:42 -0800

    Export functions

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -3,55 +3,55 @@ import fetch from 'node-fetch'
 
 const { version } = require('../package.json'); // eslint-disable-line
 
-const headers = {
-  'User-Agent': `node-yggdrasil/${version as string}`,
-  'Content-Type': 'application/json'
-}
+export async function call(host: string, path: string, data: any, agent?: Agent) {
+  const resp = await fetch(`${host}/${path}`, {
+    agent,
+    body: JSON.stringify(data),
+    headers: {
+      'User-Agent': `node-yggdrasil/${version as string}`,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  })
 
-const utils = {
-  /**
-   * Generic POST request
-   */
-  call: async function (host: string, path: string, data: any, agent: Agent | undefined) {
-    const resp = await fetch(`${host}/${path}`, { agent, body: JSON.stringify(data), headers, method: 'POST' })
-    let body: string | any = await resp.text()
-    if (body.length === 0) return ''
-    try {
-      body = JSON.parse(body)
-    } catch (e) {
-      if (e instanceof SyntaxError) {
-        if (resp.status === 403) {
-          if ((body as string).includes('Request blocked.')) {
-            throw new Error('Request blocked by CloudFlare')
-          }
-          if ((body as string).includes('cf-error-code">1009')) {
-            throw new Error('Your IP is banned by CloudFlare')
-          }
-        } else {
-          throw new Error(`Response is not JSON. Status code: ${resp.status ?? 'no status code'}`)
+  let body: string | any = await resp.text()
+  if (body.length === 0) return ''
+  try {
+    body = JSON.parse(body)
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      if (resp.status === 403) {
+        if ((body as string).includes('Request blocked.')) {
+          throw new Error('Request blocked by CloudFlare')
+        }
+        if ((body as string).includes('cf-error-code">1009')) {
+          throw new Error('Your IP is banned by CloudFlare')
         }
       } else {
-        throw e
+        throw new Error(`Response is not JSON. Status code: ${resp.status ?? 'no status code'}`)
       }
+    } else {
+      throw e
     }
-    if (body?.error !== undefined) throw new Error(body?.errorMessage)
-    return body
-  },
-  /**
-   * Java's stupid hashing method
-   * @param  {Buffer|String} hash     The hash data to stupidify
-   * @param  {String} encoding Optional, passed to Buffer() if hash is a string
-   * @return {String}          Stupidified hash
-   */
-  mcHexDigest: function (hash: Buffer | String, encoding?: String): any {
-    if (!(hash instanceof Buffer)) {
-      hash = (Buffer as any).from(hash, encoding)
-    }
-    // check for negative hashes
-    const negative = (hash as any).readInt8(0) < 0
-    if (negative) performTwosCompliment(hash)
-    return (negative ? '-' : '') + hash.toString('hex').replace(/^0+/g, '')
   }
+  if (body?.error !== undefined) throw new Error(body?.errorMessage)
+  return body
+}
+
+/**
+ * Java's stupid hashing method
+ * @param  {Buffer|String} hash     The hash data to stupidify
+ * @param  {String} encoding Optional, passed to Buffer() if hash is a string
+ * @return {String}          Stupidified hash
+ */
+export function mcHexDigest(hash: Buffer | String, encoding?: String): any {
+  if (!(hash instanceof Buffer)) {
+    hash = (Buffer as any).from(hash, encoding)
+  }
+  // check for negative hashes
+  const negative = (hash as any).readInt8(0) < 0
+  if (negative) performTwosCompliment(hash)
+  return (negative ? '-' : '') + hash.toString('hex').replace(/^0+/g, '')
 }
 
 /**
@@ -59,7 +59,7 @@ const utils = {
  * All credit to andrewrk
  * https://gist.github.com/andrewrk/4425843
  */
-function performTwosCompliment (buffer: any): void {
+function performTwosCompliment(buffer: any): void {
   let carry = true
   let i, newByte, value
   for (i = buffer.length - 1; i >= 0; --i) {
@@ -73,6 +73,3 @@ function performTwosCompliment (buffer: any): void {
     }
   }
 }
-
-
-export = utils
